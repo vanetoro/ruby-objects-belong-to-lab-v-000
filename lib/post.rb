0a281c6ed458526c1e959dcf184935d48dfb18bf
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 21:40:18 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :title, :author
+  
+  def title=(title)
+    @title = title
+  end
+  
+end
+
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+  
+  def title=(title)
+    @title = title
+  end
+
+end
